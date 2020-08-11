cfb8ef7bb3866e26126eccc4e189beadf5cd2bae
adeel1997 <adeel707@gmail.com>
2020-08-11 23:48:35 +0530

    First commit

---
+++ server.R
@@ -0,0 +1,47 @@
+library(shiny)
+library(mapview)
+library(sp)
+library(raster)
+library(rgdal)
+library(plainview)
+library(RColorBrewer)
+
+PM25 <- read.csv("data/PM25_Month.csv",col.names = c("Number","Station_Name","Location","Latitude","Longitude",
+                                                              "January","February","March","April","May","June","July","August","September","October","November","December"))
+coordinates(PM25) <- ~Longitude + Latitude  
+proj4string(PM25)=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+# Changing the Projection
+PM25_T <- spTransform(PM25, CRS("+proj=utm +zone=43 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"))
+
+# Reading the Shape file of Delhi
+Delhi <- readOGR("data/Shape file/delhi.shp")
+Delhi_T <- spTransform(Delhi, CRS("+proj=utm +zone=43 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")) 
+
+grdpts <- makegrid(Delhi_T)
+spgrd <- SpatialPoints(grdpts, proj4string = CRS(proj4string(Delhi_T)))
+spgrdWithin <- SpatialPixels(spgrd[Delhi_T,])
+
+
+shinyServer(function(input, output, session) {
+        
+    m <- reactive({
+        i <- months(input$slider)
+        x <- paste(i, "~","1")
+        formula <- as.formula(x)
+        PM25.idw <- gstat::idw(formula, PM25_T, newdata=spgrdWithin, idp=2.0)
+        raster_IDW  <- raster(PM25.idw)
+        red_colors <- brewer.pal(9, "Reds") %>%
+            colorRampPalette()
+        mapview(raster_IDW,alpha=0.4,na.color="transparent",col.regions=red_colors,legend.pos="bottomright",layer.name="PM2.5") + mapview(PM25_T, zcol = "Station_Name",legend=FALSE)
+    })
+    
+    
+    output$mapplot <- renderLeaflet({
+        m <- m()
+        m@map
+    })
+    
+})
+#PM25
+#mapviewOptions(legend.pos = "bottomright")
+#runApp(ui,shinyServer())
---
+++ ui.R
@@ -0,0 +1,34 @@
+library(shinydashboard)
+library(mapview)
+library(plainview)
+library(shiny)
+library(leaflet)
+#header <- dashboardHeader(title="Adeel Khan")
+
+#sidebar <- dashboardSidebar(
+#)
+
+#body <- dashboardBody(
+#    # Define UI for application
+#    fluidPage(
+#        sidebarPanel(
+#            sliderInput("slider", "Time", min = as.Date("2019-01-01"),max =as.Date("2019-12-31"),
+#                        value=as.Date("2019-01-01"),timeFormat="%B %Y")),
+#        mainPanel(
+#            leafletOutput("mapplot"),
+#            plainViewOutput("test")
+#        ))
+#)
+
+#ui <- dashboardPage(header, sidebar, body, skin="black")
+
+ui <- bootstrapPage(
+    tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+    titlePanel("PM2.5 Spatial Variation in Delhi 2019"),
+    leafletOutput("mapplot",width = "100%", height = "100%"),
+    plainViewOutput("test"),
+    absolutePanel(top = 10, right = 10,
+                  sliderInput("slider", "Time", min = as.Date("2019-01-01"),max =as.Date("2019-12-31"),
+                              value=as.Date("2019-01-01"),timeFormat="%B %Y")
+    )
+)
